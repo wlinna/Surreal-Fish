7e3a3210f365406ad4be3f0b3be66dc3d40e62d6
William Linna <william.linna@openmailbox.org>
2015-07-30 22:37:05 +0300

    Added simple terrain to "IlkkaMain"

--- src/surrealfish/IlkkaMain.java
+++ src/surrealfish/IlkkaMain.java
@@ -1,32 +1,17 @@
 package surrealfish;
 
-import arkhados.net.DefaultReceiver;
-import arkhados.net.OneTrueMessage;
-import arkhados.net.Receiver;
 import com.jme3.animation.AnimChannel;
 import com.jme3.animation.AnimControl;
 import com.jme3.animation.AnimEventListener;
 import com.jme3.animation.LoopMode;
 import com.jme3.app.SimpleApplication;
-import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.control.BetterCharacterControl;
 import com.jme3.input.ChaseCamera;
-import com.jme3.input.controls.ActionListener;
 import com.jme3.light.DirectionalLight;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
-import com.jme3.network.Client;
-import com.jme3.network.Network;
-import com.jme3.network.NetworkClient;
 import com.jme3.renderer.RenderManager;
-import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
-import com.jme3.scene.shape.Box;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import surrealfish.net.DataRegistration;
 
 /**
  * test
@@ -52,7 +37,16 @@ public class IlkkaMain extends SimpleApplication
         DirectionalLight dl = new DirectionalLight();
         dl.setDirection(new Vector3f(-0.1f, -1f, -1).normalizeLocal());
         rootNode.addLight(dl);
-        player = (Node) assetManager.loadModel("Models/testiUkko.j3o");        
+        
+        rootNode.attachChild(assetManager.loadModel("Scenes/newScene.j3o"));
+        
+        player = (Node) assetManager.loadModel("Models/testiUkko.j3o");
+        // Use BetterCharacterControl instead of using move-method of player
+//        player.addControl(new BetterCharacterControl(5f, 5f, 1f));
+//        player.getControl(BetterCharacterControl.class).setWalkDirection(new Vector3f(Vector3f.UNIT_Z));
+
+
+        
         rootNode.attachChild(player);
         for (Spatial object : ((Node) player.getChild(0)).getChildren()) {
             AnimControl control = object.getControl(AnimControl.class);
@@ -75,6 +69,7 @@ public class IlkkaMain extends SimpleApplication
 
     @Override
     public void simpleUpdate(float tpf) {
+        player.move(0f, 0f, 1f * tpf);
     }
 
     @Override
@@ -82,6 +77,7 @@ public class IlkkaMain extends SimpleApplication
     }
 
 
+    @Override
     public void onAnimCycleDone(AnimControl control, AnimChannel channel, String animName) {
         if (animName.equals("Walk")) {
             channel.setAnim("Walk");
@@ -91,7 +87,7 @@ public class IlkkaMain extends SimpleApplication
     }
 
  
+    @Override
     public void onAnimChange(AnimControl control, AnimChannel channel, String animName) {
-        // unused
     }
 }
