7916de012aa1a627d0b758d7c5dd4b0cdfd6f3e4
Ilkka Wirkkala <vanhailkka@hotmail.com>
2015-08-02 22:04:17 +0300

    Camera behind. And Moving.

--- src/surrealfish/IlkkaMain.java
+++ src/surrealfish/IlkkaMain.java
@@ -6,12 +6,25 @@ import com.jme3.animation.AnimEventListener;
 import com.jme3.animation.LoopMode;
 import com.jme3.app.SimpleApplication;
 import com.jme3.bullet.control.BetterCharacterControl;
+import com.jme3.font.BitmapText;
 import com.jme3.input.ChaseCamera;
+import com.jme3.input.KeyInput;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseAxisTrigger;
 import com.jme3.light.DirectionalLight;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
+import com.jme3.scene.CameraNode;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
+import com.jme3.scene.control.CameraControl.ControlDirection;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * test
@@ -21,9 +34,13 @@ import com.jme3.scene.Spatial;
 public class IlkkaMain extends SimpleApplication
         implements AnimEventListener {
 
-    //private AnimChannel channel;
+    private ArrayList<AnimChannel> channelList = new ArrayList<AnimChannel>();
     //private AnimControl control;
+    private CameraNode camNode;
+    private Vector2f oldMousePos;
+    private BitmapText hudText;
     Node player;
+    float walkTime = 0;
 
     public static void main(String[] args) {
         IlkkaMain app = new IlkkaMain();
@@ -41,42 +58,77 @@ public class IlkkaMain extends SimpleApplication
         rootNode.attachChild(assetManager.loadModel("Scenes/newScene.j3o"));
 
         player = (Node) assetManager.loadModel("Models/testiUkko.j3o");
-        // Use BetterCharacterControl instead of using move-method of player
-//        player.addControl(new BetterCharacterControl(5f, 5f, 1f));
-//        player.getControl(BetterCharacterControl.class).setWalkDirection(new Vector3f(Vector3f.UNIT_Z));
-
-
-
+        
+        
         rootNode.attachChild(player);
         Node child = (Node) player.getChild(0);
         AnimControl control1 = child.getControl(AnimControl.class);
         control1.addListener(this);
 
-//        AnimChannel channel1 = control1.createChannel();
-//        channel1.setAnim("Walk");
-        
         for (Spatial object : child.getChildren()) {
             AnimControl control = object.getControl(AnimControl.class);
             if (control == null) {
                 continue;
             }
+
             control.addListener(this);
             AnimChannel channel = control.createChannel();
             channel.setAnim("Walk");
+            channel.setSpeed(0f);
+            channelList.add(channel);
         }
 
         flyCam.setEnabled(false);
+
+        camNode = new CameraNode("Camera Node", cam);
+        camNode.setControlDir(ControlDirection.SpatialToCamera);
+
+        player.attachChild(camNode);
+        camNode.setLocalTranslation(new Vector3f(0, 5, 10));
+        camNode.lookAt(player.getLocalTranslation(), Vector3f.UNIT_Y);
+
         ChaseCamera chaseCam = new ChaseCamera(cam, player, inputManager);
         chaseCam.setSmoothMotion(true);
         //control = player.getChild("TestiUkko:Body").getControl(AnimControl.class);
         //control.addListener(this);
         //channel = control.createChannel();
         //channel.setAnim("Walk");
+
+        // Test multiple inputs per mapping
+        inputManager.addMapping("Forward", new KeyTrigger(KeyInput.KEY_W));
+        inputManager.addMapping("Backward", new KeyTrigger(KeyInput.KEY_S));
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+
+        // Test multiple listeners per mapping
+        //inputManager.addListener(actionListener, "My Action");
+        inputManager.addListener(analogListener, "Forward");
+        inputManager.addListener(analogListener, "Backward");
+        inputManager.addListener(analogListener, "Left");
+        inputManager.addListener(analogListener, "Right");
+        oldMousePos = new Vector2f(inputManager.getCursorPosition());
+        IntalizeText();
+    }
+
+    private void IntalizeText() {
+        hudText = new BitmapText(guiFont, false);
+        hudText.setSize(guiFont.getCharSet().getRenderedSize());      // font size
+        hudText.setColor(ColorRGBA.Blue);                             // font color
+        hudText.setText("You can write any string here");             // the text
+        hudText.setLocalTranslation(300, hudText.getLineHeight(), 0); // position
+        guiNode.attachChild(hudText);
+
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        player.move(0f, 0f, 1f * tpf);
+
+        
+        if (inputManager.getCursorPosition().x != oldMousePos.x) {
+            float dx = inputManager.getCursorPosition().x - oldMousePos.x;
+            player.rotate(0, -dx * 0.01f, 0);
+            oldMousePos.set(inputManager.getCursorPosition());
+        }
     }
 
     @Override
@@ -85,14 +137,63 @@ public class IlkkaMain extends SimpleApplication
 
     @Override
     public void onAnimCycleDone(AnimControl control, AnimChannel channel, String animName) {
-        if (animName.equals("Walk")) {
-            channel.setAnim("Walk");
-            channel.setLoopMode(LoopMode.DontLoop);
-            channel.setSpeed(1f);
-        }
+        //if (animName.equals("Walk")) {
+        //channel.setAnim("Walk");
+        //}
     }
 
     @Override
     public void onAnimChange(AnimControl control, AnimChannel channel, String animName) {
+        
     }
+
+    private void updateWalkTime() {
+        for (AnimChannel object : channelList) {
+            object.setTime(walkTime);
+        }
+    }
+
+    public final Vector3f getForward(Node node) {
+        Vector3f forward = new Vector3f(0, 0, -1);
+        forward = node.getLocalRotation().mult(forward);
+        return forward;
+    }
+    private ActionListener actionListener = new ActionListener() {
+        public void onAction(String name, boolean pressed, float tpf) {
+            System.out.println(name + " = " + pressed);
+        }
+    };
+    public AnalogListener analogListener = new AnalogListener() {
+        public void onAnalog(String name, float value, float tpf) {
+            if (name.equals("Forward")) {
+                Vector3f dir = getForward(player);
+                dir.multLocal(tpf);
+                player.move(dir);
+                walkTime += tpf;
+                updateWalkTime();
+            }
+
+            if (name.equals("Backward")) {
+                Vector3f dir = getForward(player);
+                dir.multLocal(-tpf);
+                player.move(dir);
+                walkTime -= tpf;
+                updateWalkTime();
+            }
+
+            if (name.equals("Right")) {
+
+
+                Vector3f dir = getForward(player).cross(Vector3f.UNIT_Y);
+                dir.multLocal(tpf);
+                player.move(dir);
+            }
+
+            if (name.equals("Left")) {
+                Vector3f dir = getForward(player).cross(Vector3f.UNIT_Y).mult(-1f);
+                dir.multLocal(tpf);
+                player.move(dir);
+            }
+        }
+    };
 }
