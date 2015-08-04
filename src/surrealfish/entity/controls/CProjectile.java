8009ef24190f5fabae73ef6e659cf527beddc1e7
William Linna <william.linna@openmailbox.org>
2015-08-04 21:32:37 +0300

    Improved correctness and reduced allocations in CProjectile

--- src/surrealfish/entity/controls/CProjectile.java
+++ src/surrealfish/entity/controls/CProjectile.java
@@ -1,4 +1,3 @@
-
 package surrealfish.entity.controls;
 
 import com.jme3.effect.ParticleEmitter;
@@ -17,34 +16,15 @@ import surrealfish.entity.ProjectileCreator;
 import surrealfish.net.commands.sync.StateData;
 
 public class CProjectile extends AbstractControl implements CSync {
-    
-    private ParticleEmitter emitter;
-    private Vector3f target = new Vector3f(0, -1, 0);
+
+    private Vector3f direction = new Vector3f(0, -1, 0);
+    private Vector3f velocity = new Vector3f();
     private float projectileSpeed = 3f;
-    
-    public CProjectile() { 
-        emitter = new ParticleEmitter("My explosion effect", ParticleMesh.Type.Triangle, 30);
-        Material mat_red = new Material(Globals.assetManager, 
-            "Common/MatDefs/Misc/Particle.j3md");
-        mat_red.setTexture("Texture", Globals.assetManager.loadTexture(
-                "Textures/flame.png"));
-        emitter.setMaterial(mat_red);
-        emitter.setImagesX(2);
-        emitter.setImagesY(2);
-        emitter.setEndColor(  new ColorRGBA(1f, 0f, 0f, 1f));
-        emitter.setStartColor(new ColorRGBA(1f, 1f, 0f, 0.5f));
-        emitter.setStartSize(.5f);
-        emitter.setEndSize(.1f);
-        emitter.setGravity(0, 0, 0);
-        emitter.setLowLife(.3f);
-        emitter.setHighLife(1f);
-        emitter.getParticleInfluencer().setVelocityVariation(.3f);
-    }
 
     @Override
     protected void controlUpdate(float tpf) {
-        spatial.move(target.normalize().mult(projectileSpeed*tpf));
-        emitter.setLocalTranslation(spatial.getLocalTranslation());
+        direction.mult(projectileSpeed * tpf, velocity);
+        spatial.move(velocity);
     }
 
     @Override
@@ -54,20 +34,35 @@ public class CProjectile extends AbstractControl implements CSync {
     @Override
     public void setSpatial(Spatial spatial) {
         super.setSpatial(spatial);
+
+        ParticleEmitter emitter = new ParticleEmitter("My explosion effect",
+                ParticleMesh.Type.Triangle, 30);
+        Material mat = new Material(Globals.assetManager,
+                "Common/MatDefs/Misc/Particle.j3md");
+        mat.setTexture("Texture", Globals.assetManager.loadTexture(
+                "Textures/flame.png"));
+        emitter.setMaterial(mat);
+        emitter.setImagesX(2);
+        emitter.setImagesY(2);
+        emitter.setEndColor(new ColorRGBA(1f, 0f, 0f, 1f));
+        emitter.setStartColor(new ColorRGBA(1f, 1f, 0f, 0.5f));
+        emitter.setStartSize(.5f);
+        emitter.setEndSize(.1f);
+        emitter.setGravity(0, 0, 0);
+        emitter.setLowLife(.3f);
+        emitter.setHighLife(1f);
+        emitter.getParticleInfluencer().setVelocityVariation(.3f);
+
         ((Node) spatial).attachChild(emitter);
     }
-    
-    
 
     @Override
     public StateData getSyncableData(StateData stateData) {
         int id = spatial.getUserData(UserData.ENTITY_ID);
         return new ProjectileCreator.ProjectileStateData(id, spatial);
     }
-    
-    public void setTarget(Vector3f target) {
-        this.target = target;
-    }    
-    
-    
+
+    public void setTarget(Vector3f dir) {
+        direction.set(dir);
+    }
 }
